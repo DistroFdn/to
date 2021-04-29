8450733181d9b8a063e2ab68f526356e7c177323
shabane <m.mohamadshabane@gmail.com>
2021-04-29 22:42:12 +0430

    clear and quit is work, menu changed again

--- to.py
+++ to.py
@@ -1,21 +1,62 @@
 #!/bin/python3
 
+import os
+import sys
+
 
 def menu():
     print("""
     help:
         
-        a   add a new to
-        i   insert a task
-        c   completion an entire to
-        d   done one task from a to
-        p   print all todo's
-        w   write to disk and exit
-        q  to quit
+        add:
+            a   add a new tag
+            i   insert a task under a tag
+
+        done:
+            c   completion an entire tag
+            d   done one task from a tag
+        
+        save and print:
+            p   print all todo's
+            s   save
+            clear clear the screen
+            
+        quit and exit:
+            w   write to disk and exit
+            q  to quit
         """)
+
+
+def clear():
+    if(sys.platform == 'linux'):
+        os.system('clear')
+    elif(sys.sys.platform == 'windows'):
+        os.system("cls")
+
+
+
+
 
 command = str()
 while(command != "q" or command != "quit"):
     command = input("command (m for menu): ")
     if(command == 'm'):
         menu()
+    elif(command == 'a'):
+        pass
+    elif(command == 'i'):
+        pass
+    elif(command == 'c'):
+        pass
+    elif(command == 'd'):
+        pass
+    elif(command == 'p'):
+        pass
+    elif(command == 's'):
+        pass
+    elif(command == 'w'):
+        pass
+    elif(command == 'q'):
+        quit()
+    elif(command == 'clear'):
+        clear()
