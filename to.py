e242a941fffdda5a31caf8893e3fe4b7343296a7
shabane <m.mohamadshabane@gmail.com>
2021-05-24 16:28:05 +0430

    ctrl + c implimented.
    fix:
                    a ctrl+c to cancel any thing.

--- to.py
+++ to.py
@@ -6,32 +6,35 @@ import sys
 command = str()
 
 while(command != "q" or command != "quit"):
-    command = input(core.TColor.NORMAL+"command (m for menu): ")
-    if(command == 'm'):
-        core.Menu()
-    elif(command == 'a'):
-        core.AddTo()
-    elif(command == 'i'):
-        core.InsertTask()
-    elif(command == 'e'):
-        core.Edit()
-    elif(command == 'c'):
-        core.CompletTo()
-    elif(command == 'd'):
-        core.DoneTask()
-    elif(command == 'u'):
-        core.UnDoneTask()
-    elif(command == 'p'):
-        core.PrintTask()
-    elif(command == 's'):
-        core.PrintDone()
-    elif(command == 'clear'):
-        core.Clear()
-    elif(command == 'q'):
-        quit()
-    elif(command == 'prog'):
-        core.Progress()
-    elif(command == 'log'):
-        pass
-    else:
-        print(core.TColor.RED+'command is incorrect')
+    try:
+        command = input(core.TColor.NORMAL+"command (m for menu): ")
+        if(command == 'm'):
+            core.Menu()
+        elif(command == 'a'):
+            core.AddTo()
+        elif(command == 'i'):
+            core.InsertTask()
+        elif(command == 'e'):
+            core.Edit()
+        elif(command == 'c'):
+            core.CompletTo()
+        elif(command == 'd'):
+            core.DoneTask()
+        elif(command == 'u'):
+            core.UnDoneTask()
+        elif(command == 'p'):
+            core.PrintTask()
+        elif(command == 's'):
+            core.PrintDone()
+        elif(command == 'clear'):
+            core.Clear()
+        elif(command == 'q'):
+            quit()
+        elif(command == 'prog'):
+            core.Progress()
+        elif(command == 'log'):
+            pass
+        else:
+            print(core.TColor.RED+'command is incorrect')
+    except KeyboardInterrupt:
+        print(core.TColor.RED+'Quit')
