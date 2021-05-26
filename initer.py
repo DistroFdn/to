b41aee149af5bbe13ea0620da354b42b0f7ee2b7
Mehran <mehranalambeigi@yahoo.com>
2021-05-26 14:08:23 +0430

    Clean the code and follow the principles of commenting and documenting

--- initer.py
+++ initer.py
@@ -2,9 +2,24 @@ import pathlib
 import os
 import json
 
+'''
+This file is responsible for creating a storage space
+on the user's path (Linux, Mac, Windows)
+to store to information and configurations
+such as username and email.
+'''
+
+# In this function, the path of the home file is specified
 def Whoami():
     return (str(pathlib.Path.home()))
 
+'''
+This function is to receive the
+user's email and username and create the file in the specified path
+of the previous function ((Whoami))
+and then save the email and username in this
+file in the form of Json
+'''
 def UserConf(username, email):
     conf_path = str(Whoami()) + '/.local/share/to.conf'
     with open(conf_path, 'w') as fli:
@@ -12,6 +27,7 @@ def UserConf(username, email):
         conf_fli = json.dumps(conf_fli, indent=4)
         fli.write(conf_fli)
 
+# In this function, the final file is created in the path.
 def IsInited():
     conf_path = str(Whoami()) + '/.local/share/to.conf'
     return os.path.exists(conf_path)
