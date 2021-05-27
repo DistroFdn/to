64efc11c58ec09bd61270bb50d08ca406d1ca209
Mehran <mehranalambeigi@yahoo.com>
2021-05-27 10:55:55 +0430

    change REDME.md and Core.py

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ git clone https://github.com/DistroTEAM/to && cd to
 - How to run
 
 > use **./to.py** to run.
-> 
+>
 > this software now is in beta version, is mean it's contain bug's. we are doing our best to make it better.
 
 ---
@@ -23,7 +23,7 @@ we wish you enjoy of it.
 
 # Features
 
-- [X] (add log)
-- [ ] (GUI app)
-- [X] (Basically)
-- [ ] (add to telegram bot)
+- [X] add log
+- [ ] GUI app
+- [X] Basically
+- [ ] add to telegram bot
--- core.py
+++ core.py
@@ -13,23 +13,27 @@ import initer
 '''
 
 def Uconf():
-    conf_path = str(initer.Whoami()) + '\.local\share\to.conf'
+    conf_path = None
+    if os.name == "nt":
+        conf_path = str(initer.Whoami()) + '\\to.conf'
+    else:
+        conf_path = str(initer.Whoami()) + '.local/share/to.conf'
     with open(conf_path, 'r') as fli:
         fli = json.load(fli)
         return (str(fli['username']), fli['email'])
 
 def GetDate():
-    check_y =True
-    while check_y:
-        deadline_y = (input(TColor.GREEN+f'Deadline Years: '))
-        if deadline_y.isdigit() and int(deadline_y) < 5 and int(deadline_y) >= 0:
-            deadline_y = int(deadline_y)
+    check_years =True
+    while check_years:
+        deadline_years = (input(TColor.GREEN+f'Deadline Years: '))
+        if deadline_years.isdigit() and int(deadline_y) < 5 and int(deadline_y) >= 0:
+            deadline_years = int(deadline_y)
             break
-        elif(deadline_y == ''):
-            deadline_y = 0
+        elif(deadline_years == ''):
+            deadline_years = 0
             break
         else:
-            print(TColor.RED+f'{deadline_y} years is too far,'
+            print(TColor.RED+f'{deadline_years} years is too far,'
                             'should be less than 5 years and greater'
                             'than or equal to 0')
 
