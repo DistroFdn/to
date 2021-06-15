721945335f8952b01178f698c09512b5b41b63d6
shabane <m.mohamadshabane@gmail.com>
2021-06-15 18:25:32 +0430

    some comment added. save the stability.
    i love her, realy. but there is no hope.

--- to.py
+++ to.py
@@ -2,17 +2,19 @@
 
 import core
 import initer
+import argparse
 
-
-
+# this method run every time that the program run, this method will check that
+# you have configed your username and emial or not. if yes you can continue to use
+#if no, method will do this for you.
 if(not initer.IsInited()):
     print(core.TColor.YELLOW + 'you shoud first config your username and email')
     username = input(core.TColor.CYAN + 'usesr name: ')
     email = input(core.TColor.CYAN + 'email: ')
     initer.UserConf(username, email)
 
+# this is the intractive command-line interface.
 command = str()
-
 while(command != "q" or command != "quit"):
     try:
         command = input(core.TColor.NORMAL+"command (m for menu): ")
